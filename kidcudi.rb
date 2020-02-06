358c745c2cae387b8049960fbc45c805422e7b42
zoraizhashmi <37843506+zoraizhashmi@users.noreply.github.com>
2020-02-06 14:38:48 -0800

    Add files via upload

--- kidcudi.rb
+++ kidcudi.rb
@@ -1,9 +1,13 @@
 # Loading previous buffer contents. Please wait...
+x = 0
 sample "C:/Users/zoraiz_hashmi/Documents/myproducertag.wav", amp: 5
 
 sleep 4.7
 
 live_loop :loop1 do
+  cray_notes = [:Eb, :G, :c5, :d5, :Bb, :C5, :a, :f, :g5]
+  cray_sleeps = [:Eb, :G, :c5, :d5, :Bb, :C5, :a, :f, :g5]
+  index = 0
   play:Eb
   sleep 0.5
   play:G
@@ -24,9 +28,13 @@ live_loop :loop1 do
   sleep 1.5
 end
 
-sleep 10.4
+sleep 10.39
 
 live_loop :bass do
   sample "C:/Users/zoraiz_hashmi/Documents/kidcudibass.wav", amp: 2.5
-  sleep 5.19
+  sleep 5.2
+end
+
+live_loop :vocals do
+  print = "they tryna be cray"
 end
